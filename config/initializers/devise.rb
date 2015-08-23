029ab13721a49aa6fc0af9a6959c50c5e833be1a
Yu Hoshino <yuhoshino@dev.local>
2015-08-24 04:01:19 +0900

    Generated User Model (not tested yet)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,3 @@
-# Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -208,7 +206,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
