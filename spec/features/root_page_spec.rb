208e0bc0b16a0e82b8c4f309618a2753536bfcdc
Yu Hoshino <yuhoshino@dev.local>
2015-08-25 21:39:20 +0900

    Added title to locale file

--- spec/features/root_page_spec.rb
+++ spec/features/root_page_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'Root', type: :feature, js: true do
   context 'shows' do
+    before { visit root_path }
+    it 'correct title' do
+      expect(page.title).to eq(I18n.t('title'))
+    end
+
     it 'Login form' do
-      visit root_path
       expect(page).to have_text('Log in')
     end
   end
