d0dd229b30ff36c970fdfac228efdc41d39583c0
Yu Hoshino <yuhoshino@dev.local>
2015-08-27 01:30:19 +0900

    close #15

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,10 @@ class ProfilesController < ApplicationController
     @profile = current_user.build_profile
   end
 
+  def edit
+    @profile = current_user.profile
+  end
+
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save
@@ -16,9 +20,18 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def update
+    @profile = current_user.profile
+    if @profile.update_attributes(profile_params)
+      redirect_to profile_path(current_user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def profile_params
-    params.require(:profile).permit(:avatar)
+    params.require(:profile).permit(:avatar, :name)
   end
 end
---
+++ spec/features/username_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Username', type: :feature, js: true do
+  context 'is needed for each user' do
+    it 'for profile' do
+      @user = create(:user)
+      login_as(@user)
+      visit root_path
+      click_link I18n.t('profile.link')
+      click_link I18n.t('profile.avatar.new')
+      fill_in 'profile_name', with: 'yhoshino11'
+      attach_file('profile_avatar',
+                  Rails.root.join('spec', 'support', 'avatar.png'))
+      click_button I18n.t('profile.avatar.upload')
+      expect(current_path).to eq(profile_path(@user))
+
+      click_link I18n.t('profile.edit')
+      expect(current_path).to eq(edit_profile_path(@user))
+      fill_in 'profile_name', with: 'yhoshino11'
+      click_button I18n.t('profile.submit')
+      expect(current_path).to eq(profile_path(@user))
+      expect(page).to have_text('yhoshino11')
+    end
+  end
+end
