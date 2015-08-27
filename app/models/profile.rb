ca6a36323f699d74f8d09c9b9165be12a395f0a2
Yu Hoshino <yuhoshino@dev.local>
2015-08-27 15:31:21 +0900

    Allow User to upload PNG/JPEG image for Profile Picture

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -16,6 +16,8 @@
 class Profile < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' },
                              default_url: '/images/:style/missing.png'
-  validates_attachment_content_type :avatar, content_type: 'image/png'
+  validates_attachment_content_type :avatar,
+                                    content_type: ['image/png',
+                                                   'image/jpeg']
   belongs_to :user
 end
