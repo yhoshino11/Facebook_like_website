52690b64c6fed52629518cc5ac05af29e0f127eb
Yu Hoshino <yuhoshino@dev.local>
2015-08-30 20:52:58 +0900

    Sleep 1 second for test.

--- spec/features/username_spec.rb
+++ spec/features/username_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Username', type: :feature, js: true do
   context 'is needed for each user' do
-    before { User.destroy_all }
-    it 'for profile' do
+    before do
+      User.destroy_all
       @user = create(:user)
       login_as(@user)
       visit root_path
+    end
+
+    it 'for profile' do
       click_link I18n.t('profile.empty')
       fill_in 'profile_name', with: 'yhoshino11'
       attach_file('profile_avatar',
@@ -15,6 +18,7 @@ RSpec.describe 'Username', type: :feature, js: true do
       expect(current_path).to eq(profile_path(@user))
 
       click_link I18n.t('profile.edit')
+      sleep 1
       expect(current_path).to eq(edit_profile_path(@user))
 
       fill_in 'name', with: 'yhoshino11'
